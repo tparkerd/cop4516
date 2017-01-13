7a1a5c9b6a232e9558f83f972706c9d547e9decb
Tim <tim.parkerd@gmail.com>
2017-01-13 17:19:30 -0500

    input is correctly read in

--- hexagon/hexagon.java
+++ hexagon/hexagon.java
@@ -2,24 +2,31 @@ import java.util.*;
 
 
 public class hexagon {
-  public int CASE_LENGTH = 42;
+  public static int NUM_SIDES = 6;
+  public static int NUM_PIECES = 7;
 
   public static void main(String[] args) {
     Scanner stdin = new Scanner(System.in);
 
     // Get number of test cases
-    int n = Integer.parseInt(stdin.nextLine());
+    int n = stdin.nextInt();
     System.out.println("Number of test cases: " + n);
 
     // For each test case...
     for (int i = 1; i <= n; i++) {
-      // Scan in the test cases
       System.out.println("Case " + i + ": ");
-      String tmpCase = stdin.nextLine();
-      String[] tmpCases = tmpCase.split("\\s");
-      System.out.println(tmpCase);
-      System.out.println(tmpCase.toString());
-      // Convert all the strings to integers
+
+      int[][] pieces = new int[NUM_PIECES][NUM_SIDES];
+      // Read in the digits of each piece
+      for (int j = 0; j < NUM_PIECES; j++) {
+        System.out.printf("Piece #" + (j + 1) + ": ");
+        for (int k = 0; k < NUM_SIDES; k++) {
+          pieces[j][k] = stdin.nextInt();
+          System.out.printf("%d ", pieces[j][k]);
+        }
+        System.out.println();
+      }
+      System.out.println();
     }
   }
 
@@ -31,5 +38,10 @@ public class hexagon {
 
     int[] tmp = { 1, 2, 3, 4, 5, 6 };
     return tmp;
+  }
+
+  public static void solve(int[][] caseNumbers) {
+    // Pick the center piece, one out of the seven, and fix it into place
+    
   }
 }
