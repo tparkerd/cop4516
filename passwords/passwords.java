be646ec5e30576e881f3dacf5f3f705b9e9a1a21
Tim <tim.parkerd@gmail.com>
2017-01-13 15:35:06 -0500

    attempting to optimize finding the password

--- passwords/passwords.java
+++ passwords/passwords.java
@@ -2,6 +2,9 @@ import java.util.*;
 
 public class passwords {
   public static ArrayList<String> passwordList;
+  public static int callCount = 0;
+  public static int passwordCount;
+  public static int rank;
 
   public static void main(String[] args) {
     Scanner stdin = new Scanner(System.in);
@@ -14,6 +17,10 @@ public class passwords {
       // Empty the password list for each case
       passwordList = new ArrayList<String>();
 
+      // Password counter: this is used to stop solving once the desired
+      // rank is reached
+      passwordCount = 0;
+
       // Get the length of the password
       int length = Integer.parseInt(stdin.nextLine());
 
@@ -21,50 +28,49 @@ public class passwords {
       // subsets can hold up to 26 lowercase letters
       char[][] sets = new char[length][26];
 
-
       // Get the set of letters for each position
       for (int j = 0; j < length; j++) {
-
         // Get set of letters and then convert to character array
         String set = stdin.nextLine();
         char[] setChars = set.toCharArray();
         sets[j] = setChars;
-
       }
-    System.out.printf("Level\tChild\tString\tSets[level].length\n");
-      buildPasswords(sets, 0, 0, new String());
+
+      // Populate the list of passwords
+      buildPasswords(sets, 0, new String());
 
       // Get rank of password
-      int rank = Integer.parseInt(stdin.nextLine());
+      rank = Integer.parseInt(stdin.nextLine());
+
+      System.out.println(passwordList.get(rank - 1));
     }
   }
 
-  // Not sure if this should be...
-  // I want to pass the set of sets of character, so I can just tick off
-  // which set has been used, so after the first set of letters is used,
-  // the currentSet would increment.
-  public static void buildPasswords(char[][] sets, int level, int child, String str) {
-    System.out.printf("%d\t%d\t%s\t", level, child, str);
+  // Solver function
+  public static void buildPasswords(char[][] sets, int level, String str) {
+    // Base case: if we already found the password at the requested rank,
+    // stop solving for more passwords
+    if (passwordCount > rank)
+      return;
+
+
     // Base case: all sets have been used, the string is created, add to
     // the master password list
+
     if (level >= sets.length) {
-      System.out.println("Password: " + str);
+      System.out.println(str);
+      passwordList.add(str);
+      passwordCount++;
       return;
     }
 
-    System.out.printf("%c\n", sets[level][child]);
-    // Base case: all children elements (letters) used in the current level
-    // Start at the first child/element of the next set
+    // Given a valid level, make candidate prefixes using its children letters
 
-    // So long as the current level has letters in its set, add them one by one
-    // but make sure to move to the next level
+    // Add each letter of the current set to create a candidate password
+    int child = 0;
     while (child < sets[level].length) {
-      buildPasswords(sets, level, child + 1, str + String.valueOf(sets[level][child]));
+      buildPasswords(sets, level + 1, str + String.valueOf(sets[level][child]));
+      child++;
     }
-
-    // Otherwise, add the next possible letter
-    // buildPasswords(sets, level + 1, 0, str + String.valueOf(sets[level][child]));
   }
-
-
 }
