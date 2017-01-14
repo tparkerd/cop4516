27ad5765515dcbb795767b1ce755990303ce5a6d
Tim <tim.parkerd@gmail.com>
2017-01-13 21:05:57 -0500

    clean up and try to establish base cases

--- hexagon/hexagon.java
+++ hexagon/hexagon.java
@@ -4,12 +4,14 @@ import java.util.*;
 public class hexagon {
   public static int NUM_SIDES = 6;
   public static int NUM_PIECES = 7;
+  public static enum Position {
+    north, northeast, southeast, south, southwest, northwest
+  }
   public static boolean[] usedPieces;
   public static int[] candidateAnswer;
 
   public static void main(String[] args) {
     Scanner stdin = new Scanner(System.in);
-
 
     // Get number of test cases
     int n = stdin.nextInt();
@@ -17,7 +19,7 @@ public class hexagon {
 
     // For each test case...
     for (int i = 1; i <= n; i++) {
-      System.out.println("Case " + i + ": ");
+      System.out.printf("Case " + i + ": ");
       usedPieces = new boolean[7];
       System.out.println(Arrays.toString(usedPieces));
 
@@ -34,23 +36,17 @@ public class hexagon {
       System.out.println();
 
       // SOLVE HERE
-      solve(pieces);
+      solveRec(pieces, new boolean[7], 0, new int[7]);
     }
   }
 
 
-  public static int[] rotatePiece(int[] piece) {
-    // Rotate the piece N times
-    // I'm not sure if it's best to rotate the original or to just
-    // make copies and rotate it N number of times as an argument
-
-    int[] tmp = { 1, 2, 3, 4, 5, 6 };
-    return tmp;
-  }
-
-  public static void rotateOneToTopFace(int[] piece) {
+  // Rotate a piece until a chosen value is on the target face of the piece
+  // E.g., rotatePiece(piece, 1, 0) would rotate the piece so that 1 would be
+  // on the top (north) face of the hexagon.
+  public static void rotatePiece(int[] piece, int value, int desiredPosition) {
     // Continue to rotate the piece until 1 is placed at the top
-    while (piece[0] != 1) {
+    while (piece[desiredPosition] != value) {
 
       // Save the last face of the piece
       int last = piece[piece.length - 1];
@@ -64,27 +60,71 @@ public class hexagon {
     }
   }
 
-  public static void solve(int[][] pieces) {
-    // Pick the center piece, one out of the seven, and fix it into place.
-    // Do this for each piece.
-    for (int i = 0; i < pieces.length; i++) {
-      // Pick piece
-      int[] piece = pieces[i];
+  public static void printOrder(int[] list) {
+    int i;
+    for (i = 0; i < list.length - 1; i++)
+      System.out.printf(list[i] + " ");
+    System.out.println(list[i]);
+  }
 
-      // Rotate it so that 1 is at the zeroth index (i.e., top of the hexagon)
-      rotateOneToTopFace(pieces[i]);
-      System.out.println("Piece #" + i + " rotated: " + Arrays.toString(pieces[i]));
+  // Validity Check Function
+  public static boolean isValid(int[][] pieces, int[] order) {
 
-      //
-      usedPieces[i] = true;
-      System.out.println(Arrays.toString(usedPieces));
+    return true;
+  }
 
-
-      // Place piece down in center position
-
-
-
+  // Recursive approach to solving the problem
+  //  pieces: a list of the pieces
+  //  used:   a list of used pieces (i.e., [T,F,F,T,F,F])
+  //          Where the 1st and 4th pieces have been placed.
+  //  placed: A counter of how many pieces have been placed
+  //          This will start at zero, which will actually equate to the
+  //          index of where the next piece should be placed. So make sure
+  //          that this gets incremented on each call to place the next piece.
+  //  TODO(timp): order: I'm not quite sure how to use this yet, but I need
+  //                     something that will keep track of the partial solution
+  //                     until solved.
+  public static void solveRec(int[][] pieces, boolean[] used, int placed, int[] order) {
+    // Base case: all pieces placed, we have a solution
+    if (placed >= NUM_PIECES) {
+      printOrder(order);
+      return;
     }
 
+    // While there is an unused piece
+    int i = 0;
+    while (used[i]) {
+      // Piece the next available piece and place it, continue on...
+
+      // Check if the placement is valid
+      if (isValid(pieces, order))
+        solveRec(pieces, used, placed + 1, order);
+      else {
+        System.out.println("No solution.");
+        return;
+      }
+
+      i++;
+    }
+
+
+    //
+    // for (i = 0; i < pieces.length; i++) {
+    //   // Pick piece
+    //   int[] piece = pieces[i];
+    //
+    //   // Rotate it so that 1 is on the northern face (i.e., top of the hexagon)
+    //   rotatePiece(pieces[i], 1, Position.north.ordinal());
+    //   System.out.println("Piece #" + i + " rotated: " + Arrays.toString(pieces[i]));
+    //
+    //   // Mark the current starting piece as used
+    //   usedPieces[i] = true;
+    //   System.out.println(Arrays.toString(usedPieces));
+    //
+    //
+    //   // Place piece down in center position
+    //
   }
+
+
 }
