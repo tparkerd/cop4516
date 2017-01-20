d05869cad831427b71ec851d01887bbd12d4714d
Tim <tim.parkerd@gmail.com>
2017-01-20 17:52:26 -0500

    updates

--- conference/conference.java
+++ conference/conference.java
@@ -1,15 +1,19 @@
-import java.util.Scanner;
-import java.util.Collections;
-import java.util.ArrayList;
+import java.util.*;
 
 public class conference {
+  public static final boolean DEBUG = true;
   public static ArrayList<Conference> schedule;
 
   public static void main(String[] args) {
 
+    Long tmp1 = Long.MAX_VALUE();
+
     Scanner stdin = new Scanner(System.in);
     int nCases = stdin.nextInt();
     for (int i = 0; i < nCases; i++) {
+      schedule = new ArrayList<Conference>();
+
+
       // Get number of conferences
       int nConf = stdin.nextInt();
       for (int j = 0; j < nConf; j++) {
@@ -18,32 +22,59 @@ public class conference {
         Conference tmp = new Conference(start, duration);
         schedule.add(tmp);
       }
-      // Sort the damn things by their ending time
+      // Sort by starting time
       Collections.sort(schedule);
+      if (DEBUG) System.out.println(schedule.toString());
+
+
+      // Process case
+      // Assume the earliest starting conference goes first
+      long total = 0;
+      int lastEndingTime = 0;
+
+      // Go through conferences
+      for (Conference c : schedule) {
+        if (c.start >= lastEndingTime) {
+          total += c.worth;
+        }
+      }
+
+      if (DEBUG) System.out.println("Earned: " + total);
+
     }
   }
 }
 
-abstract class Conference implements Comparable<Conference> {
+class Conference implements Comparable<Conference> {
+  int start;
   int end;
   int duration;
-  int value;
+  long worth;
 
   public Conference(int start, int duration) {
+    this.start = start;
     this.duration = duration;
     this.end = start + duration;
-    this.value = 0;
-    for (int i = 1; i <= duration; i++) {
-      this.value += (2e29) / (2 * i);
+    this.worth = 0;
+    for (int i = start; i <= duration; i++) {
+      this.worth += (2e29) / (2 * i);
+      // this.worth += this.duration;
     }
   }
 
-  public int compareTo(Conference c, Conference o) {
-    if (c.end > o.end)
-      return 1;
-    else if (c.end < o.end)
+  @Override
+  public int compareTo(Conference o) {
+    if (o.start > this.start)
       return -1;
+    else if (o.start < this.start)
+      return 1;
     else
       return 0;
+  }
+
+  public String toString() {
+    return new String("\nStart: " + this.start + "\nEnd: " + this.end +
+                      "\nDuration: " + this.duration + "\nWorth: " +
+                      this.worth + "\n");
   }
 }
