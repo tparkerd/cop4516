a558f449c657da7eb154160b40c65b3e41f17ef5
Tim <tim.parkerd@gmail.com>
2017-01-19 20:06:45 -0500

    initial container commit

---
+++ containers/containers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class containers {
+  public static void main(String[] args) {
+
+    Scanner stdin = new Scanner(System.in);
+
+    String tmpCase = new String();
+    while (stdin.hasNextLine()) {
+      tmpCase = stdin.nextLine();
+      System.out.println(tmpCase);
+    }
+
+  }
+
+
+
+}
