4f9b157c5827dd903472ce7af9d5f8a5869b3628
Tim <tim.parkerd@gmail.com>
2017-01-20 16:08:12 -0500

    update barebones of containers, still incomplete

--- containers/containers.java
+++ containers/containers.java
@@ -2,14 +2,27 @@ import java.util.Scanner;
 
 
 public class containers {
+  public final static boolean DEBUG = true;
+
   public static void main(String[] args) {
 
     Scanner stdin = new Scanner(System.in);
 
-    String tmpCase = new String();
+    // Get each case and process it
+    String tmpCase;
     while (stdin.hasNextLine()) {
+      // Get the next case
       tmpCase = stdin.nextLine();
-      System.out.println(tmpCase);
+      // If the end of the file was reached, stop processing cases
+      if (tmpCase.equals("end")) break;
+      if (DEBUG) System.out.println(tmpCase);
+
+      
+
+
+
+
+
     }
 
   }
