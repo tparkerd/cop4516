2dbff93e83e3b8e1d1e98ff927041e54bae164bb
Tim <Tim.ParkerD@gmail.com>
2017-01-20 11:51:00 -0500

    need to learn how to implement Comparable again

--- conference/conference.java
+++ conference/conference.java
@@ -1,7 +1,10 @@
 import java.util.Scanner;
 import java.util.Collections;
+import java.util.ArrayList;
 
 public class conference {
+  ArrayList<Conference> schedule;
+
   public static void main(String[] args) {
 
     Scanner stdin = new Scanner(System.in);
@@ -9,7 +12,11 @@ public class conference {
     for (int i = 0; i < nCases; i++) {
       // Get number of conferences
       int nConf = stdin.nextInt();
-
+      for (int j = 0; j < nConf; j++) {
+        int start = stdin.nextInt();
+        int duration = stdin.nextInt();
+        Conference tmp = new Conference(start, duration);
+      }
     }
 
 
@@ -18,17 +25,21 @@ public class conference {
 
 }
 
-abstract class Conference implements Comparable<Conference> {
+abstract class Conference implements Comparable<Object> {
   int end;
   int duration;
 
-  public Conference(int start, int end) {
-    this.end = end;
-    this.duration = end - start;
+  public Conference(int start, int duration) {
+    this.duration = duration;
+    this.end = start + duration;
   }
 
-  public void compareTo(Conference c, Conference o) {
+  public int compareTo(Conference c, Conference o) {
     if (c.end > o.end)
-      ;
+      return 1;
+    else if (c.end < o.end)
+      return -1;
+    else
+      return 0;
   }
 }
