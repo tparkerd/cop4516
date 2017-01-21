1b2ceb6521d139bdba5e694149e81fc39170d86a
Tim <tim.parkerd@gmail.com>
2017-01-21 14:41:08 -0500

    update diamonds to just read in all the diamonds

--- diamond/diamond.java
+++ diamond/diamond.java
@@ -2,10 +2,7 @@ import java.util.*;
 import java.math.*;
 
 public class diamond {
-  public static final boolean DEBUG = false;
-
-  public static DiamondCase case1;
-  public static DiamondCase case2;
+  public static final boolean DEBUG = true;
 
   public static void main(String[] args) {
     // Read in the data
@@ -15,60 +12,15 @@ public class diamond {
 
     // For each case...
     for (int i = 0; i < nCases; i++) {
-      case1 = new DiamondCase();
-      case2 = new DiamondCase();
-      // Number of diamonds
-      int nDiamonds = stdin.nextInt();
-      // Max size difference
-      int k = stdin.nextInt();
-      // For each diamond...
-      for (int j = 0; j < nDiamonds; j++) {
-        int diamond = stdin.nextInt();
+      // Get the case details
+      int nDiamonds = stdin.nextInt();      // Number of diamonds
+      int k = stdin.nextInt();              // Max size difference
+      int[] diamonds = new int[nDiamonds];
+      // Add all the diamonds to a list
+      for (int j = 0; j < nDiamonds; j++)
+        diamonds[j] = stdin.nextInt();
 
-        // If no diamond has been placed, place the first one in the first case
-        if (j == 0) {
-            case1.list.add(diamond);
-            continue; // to go the next case, although this is unnecessary
-        }
-
-        // Otherwise, start testing who goes where
-        // If the diamond can fit in the first case, add( it there, otherwise
-        // try case 2, if not, skip that diamond
-        if (case1.canFit(diamond, k)) {
-          case1.list.add(diamond);
-        } else if (case2.canFit(diamond, k)) {
-          case2.list.add(diamond);
-        }
-      }
-
-      int diamondsPlaced = case1.list.size() + case2.list.size();
-      System.out.println(diamondsPlaced);
+      if (DEBUG) System.out.println(Arrays.toString(diamonds));
     }
-  }
-}
-
-class DiamondCase {
-  ArrayList<Integer> list;
-
-  public DiamondCase() {
-    list = new ArrayList<Integer>();
-  }
-
-  // See if the current diamond can fit in
-  public boolean canFit(int n, int k) {
-
-    // Base case: case is empty, it can fit
-    if (this.list.size() == 0) return true;
-
-    // See if the new diamond is within K values of each diamond already in the case
-    for (int i = 0;i < this.list.size(); i++) {
-      // If a diamond exceeds K difference between them, this cannot be added
-      if (Math.abs(list.get(i) - n) > k) return false;
-    }
-
-    return true;
-
-    // System.out.println(this.list.toString());
-    // if (Math.abs(this.list.get(0) - n) > k) result = false;
   }
 }
