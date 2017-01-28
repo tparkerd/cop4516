7d5349dda33b9159edb1bc01c4eb4778e3a1e53c
Tim <tim.parkerd@gmail.com>
2017-01-28 15:59:33 -0500

    cleaned up and removed debug statements

--- treesales/treesales.java
+++ treesales/treesales.java
@@ -3,13 +3,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class treesales {
-  public static final boolean DEBUG = false;
-  public static final String ADD   = "ADD";
-  public static final String SALE  = "SALE";
-  public static final String QUERY = "QUERY";
-  public static final String ROOT  = "ROOT";
-  public static Person root;
-  public static HashMap<String, Person> myHash;
+  // Quick references to all Person objects
+  public static HashMap<String, Person> employees;
 
   public static void main(String[] args) {
     Scanner stdin = new Scanner(System.in);
@@ -17,56 +12,48 @@ public class treesales {
     int nCases = Integer.parseInt(stdin.nextLine());
     for (int i = 0; i < nCases; i++) {
       System.out.println("COMPANY " + (i + 1));
-      // Create an empty tree to act upon
-      root = null;
 
       // Create a new hash map for quick access to all persons
-      myHash = new HashMap<String, Person>();
+      employees = new HashMap<String, Person>();
 
       // Get the number of commands to execute
       int nCommands = Integer.parseInt(stdin.nextLine());
       for (int j = 0; j < nCommands; j++) {
         String tmpCmd = stdin.nextLine();
-        // if (DEBUG) System.out.println(tmpCmd);
 
         // Parse the command
         String[] cmd = tmpCmd.split(" ");
 
-        // Determine the type of command
+        // Placeholder variable for creating new employees or holding a reference
         Person tmp;
+        // Determine the type of command
         switch (cmd[0]) {
-          case ADD:
+          case "ADD":
             // Find the boss
-            Person boss = myHash.get(cmd[1]);
+            Person boss = employees.get(cmd[1]);
             // If null, that means this is the CEO
             if (boss == null) {
-              if (DEBUG) System.out.println("Create CEO");
-              root = new Person(null, cmd[2]);
-              myHash.put(root.name, root);
+              tmp = new Person(null, cmd[2]);
+              employees.put(tmp.name, tmp);
             // Otherwise, this is a normal employee, so just add them
             // under the correct boss and get there address to hash back
             // into the hash map
             } else {
-              if (DEBUG) System.out.println("Add user: " + cmd[2]);
+              // Hire them as a subordinate to the current person
               tmp = boss.add(cmd[2]);
-              myHash.put(tmp.name, tmp);
-              if (DEBUG) System.out.println("Hire " + cmd[2] + " under " + myHash.get(cmd[2]).boss.name);
+              // Store a reference to them for quick access later
+              employees.put(tmp.name, tmp);
             }
             break;
-          case SALE:
-            if (DEBUG) System.out.println("New SALE: " + cmd[1] + " sold " + cmd[2]);
+          case "SALE":
             // Get the person that made the sale
-            tmp = myHash.get(cmd[1]);
+            tmp = employees.get(cmd[1]);
+            // Recursively add this amount to their sale amount and their higher-ups
             tmp.sale(Integer.parseInt(cmd[2]));
             break;
-          case QUERY:
-            if (DEBUG) System.out.println("New QUERY: " + cmd[1]);
-            // Get a reference to the person
-            tmp = myHash.get(cmd[1]);
-            System.out.println(tmp.sales);
-            break;
-          default:
-            if (DEBUG) System.out.println("ERROR. INVALID COMMAND");
+          case "QUERY":
+            // Get a reference and then access the person's sale amount
+            System.out.println(employees.get(cmd[1]).sales);
             break;
         }
       }
@@ -88,9 +75,6 @@ class Person {
   }
 
   public Person add(String name) {
-    // Unnecessary, but see if they already exist in the hash map
-    // TODO(timp): see if they are already in the hash map
-
     // Create a new person
     Person tmp = new Person(this, name);
     // Hire them under the current person
