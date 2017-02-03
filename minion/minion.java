4a5a24a7b9576e7761009da21770f8f94d7de82a
Tim <Tim.ParkerD@gmail.com>
2017-02-03 09:46:41 -0500

    initial files

---
+++ minion/minion.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class minion {
+  public static final boolean DEBUG = true;
+
+  public static void main(String[] args) {
+    Scanner stdin = new Scanner(System.in);
+
+  }
+}
