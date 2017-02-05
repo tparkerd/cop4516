e2b72616664d75a8c33c127052102fc429ddf8d2
Tim <tim.parkerd@gmail.com>
2017-02-05 18:00:56 -0500

    Board preview works

--- puzzle/puzzle.java
+++ puzzle/puzzle.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class puzzle {
+  public static final boolean DEBUG = true;
+  public static final int SIZE = 9;
   public static void main(String[] args) {
     Scanner stdin = new Scanner(System.in);
 
@@ -13,18 +15,28 @@ public class puzzle {
       long[] states = new long[10000];
       // Get the board
       // Since the values are only up to 8, I should be able to store them in just 4 bits
-      long curState = 0;
-      for (int j = 0; j < 8; j++) {
-        curState = (curState << 4) + stdin.nextInt();
-      }
-      System.out.println(curState);
+      // Read in the initial state of the board
+      State tmpState = new State(0);
+      for (int j = 0; j < SIZE; j++)
+        tmpState.id = (tmpState.id << 4) + stdin.nextInt();
+
+      // Debug preview of the board
+      if (DEBUG) tmpState.view();
+
+
+      // We know it is solved when it is in the correct
+      if(tmpState.isSolved())
+        System.out.println("Solved!");
+      else
+        System.out.println("Not solved yet.");
     }
 
   }
 }
 
 class State {
-  long id;
+  public long id;
+  public static final long SOLVED_STATE = 4886718336L;
 
   public State(long l) {
     this.id = l;
@@ -32,7 +44,17 @@ class State {
 
   // Debug display of the board
   public void view() {
-    
+    long[] tmp = new long[puzzle.SIZE];
+    for (int i = 0; i < puzzle.SIZE; i++) {
+      tmp[puzzle.SIZE - 1 - i] = 0xF & (this.id >> (i * 4));
+    }
+    System.out.printf("[%d][%d][%d]\n[%d][%d][%d]\n[%d][%d][%d]\n",
+                        tmp[0], tmp[1], tmp[2],
+                        tmp[3], tmp[4], tmp[5],
+                        tmp[6], tmp[7], tmp[8]);
+  }
 
+  public boolean isSolved() {
+    return (this.id == SOLVED_STATE);
   }
 }
