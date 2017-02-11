83d36760eb5c5dddd350b62ff094af05c27a8f36
Tim <tim.parkerd@gmail.com>
2017-02-11 17:57:47 -0500

    one array bounds error fixed

--- relatives/relatives.java
+++ relatives/relatives.java
@@ -1,42 +1,33 @@
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
 
 public class relatives {
   public static int[][] G;
   public static int[][] D;
-  public static int[][] P;
-  public static int nPeople;
-  public static HashMap<String, Integer> map;
-  public static final int oo = Integer.MAX_VALUE;
+  public static int N;
+  public static int R;
+  public static HashMap<String, Integer> myHashMap;
+  public static final int oo = 1000;
 
   public static void main(String[] args) {
-
     Scanner stdin = new Scanner(System.in);
 
     int caseCounter = 1;
-    while (stdin.hasNext()) {
-      // Get the people count and relationship count
-      nPeople = Integer.parseInt(stdin.next());
-      int nRelationships = Integer.parseInt(stdin.next());
-
-
-      // EOF condition
-      if (nPeople == 0 || nRelationships == 0) break;
+    // Get the people count and relationship count
+    N = Integer.parseInt(stdin.next().trim());
+    R = Integer.parseInt(stdin.next().trim());
+    while (N != 0 && R != 0) {
 
       // Get all the relationships
-      String[] relationships = new String[nRelationships * 2];
-      for (int i = 0; i < nRelationships * 2; i++) {
-        relationships[i] = stdin.next();
-        relationships[++i] = stdin.next();
+      String[] relationships = new String[R * 2];
+      for (int i = 0; i < (R * 2); i++) {
+        relationships[i] = stdin.next();    // Get the current one (k)
+        relationships[++i] = stdin.next();  // Get the next (k + 1) and the go to (2k)
       }
 
-      System.out.println(Arrays.toString(relationships));
-
       // New graph to connect the people
-      G = new int[nPeople][nPeople];
+      G = new int[N][N];
       // New hash map to store people as indices
-      map = new HashMap<String, Integer>();
+      myHashMap = new HashMap<String, Integer>();
 
       // Initialize all relationships to infinity
       for (int[] row : G)
@@ -46,23 +37,27 @@ public class relatives {
       int indexCounter = 0;
       for (String str : relationships) {
         // Person not already mapped
-        if (!map.containsKey(str)) {
-          map.put(str, indexCounter++);
+        if (!myHashMap.containsKey(str)) {
+          myHashMap.put(str, indexCounter);
+          indexCounter++;
         }
       }
 
       // A person has no distance from itself
-      for (int i = 0; i < nRelationships; i++)
-        for (int j = 0; j < nRelationships; j++)
-          if (i == j)
+      for (int i = 0; i < R; i++) {
+        for (int j = 0; j < R; j++) {
+          if (i == j) {
             G[i][j] = 0;
+          }
+        }
+      }
 
 
       // Parse out the relationships from the relationships buffer
       // These are the edges on the G
       for (int i = 0; i < relationships.length - 1; i++) {
-        int start = map.get(relationships[i]);
-        int end = map.get(relationships[++i]);
+        int start = myHashMap.get(relationships[i]);
+        int end = myHashMap.get(relationships[++i]);
         G[start][end] = 1;
         G[end][start] = 1;
       }
@@ -76,6 +71,9 @@ public class relatives {
       } else {
         System.out.println(result);
       }
+      // Get the people count and relationship count
+      N = Integer.parseInt(stdin.next().trim());
+      R = Integer.parseInt(stdin.next().trim());
       caseCounter++;
     }
   }
@@ -83,29 +81,18 @@ public class relatives {
 
 
   public static void floyd() {
-    P = new int[nPeople][nPeople];
-    for (int i = 0; i < nPeople; ++i) {
-      for (int j = 0; j < nPeople; ++j) {
-        if (G[i][j] < oo)
-          P[i][j] = i;
-        else
-          P[i][j] = -1;
-      }
-    }
-
-    D = new int[nPeople][nPeople];
-    for (int i = 0; i < nPeople; i++) {
-      for (int j = 0; j < nPeople; j++) {
+    D = new int[N][N];
+    for (int i = 0; i < N; ++i) {
+      for (int j = 0; j < N; ++j) {
         D[i][j] = G[i][j];
       }
     }
 
-    for (int k = 0; k < nPeople; k++) {
-      for (int i = 0; i < nPeople; i++) {
-        for (int j = 0; j < nPeople; j++) {
+    for (int k = 0; k < N; ++k) {
+      for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
           if ((D[i][j] > D[i][k] + D[k][j]) && (D[i][k] < oo) && (D[k][j] < oo)) {
             D[i][j] =  D[i][k] + D[k][j];
-            P[i][j] = P[k][j];
           }
         }
       }
@@ -114,8 +101,8 @@ public class relatives {
 
   public static int findMax() {
     int max = Integer.MIN_VALUE;
-    for (int i = 0; i < nPeople; i++) {
-      for (int j = 0; j < nPeople; j++) {
+    for (int i = 0; i < N; ++i) {
+      for (int j = 0; j < N; ++j) {
         if (D[i][j] > max) {
           max = D[i][j];
         }
