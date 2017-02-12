236bc270a42552e3a7e1cde7010e96b243ca6600
Tim <tim.parkerd@gmail.com>
2017-02-12 13:48:34 -0500

    template file

---
+++ usaco/template.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+public class ____________ {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader("____________.in"));
+		PrintWriter pw = new PrintWriter(new BufferedWriter(new FileWriter("____________.out")));
+
+		pw.close();
+	}
+}
