cc74562e4de42e03900047f268f7b68d51de7afe
Tim <tim.parkerd@gmail.com>
2017-02-12 22:50:23 -0500

    solution candidate that optimized to find the least required fixes necessary. However, this is still a bit of doubt on how to best generate the perms of fixed crosswalks

--- usaco/maxcross/maxcross.java
+++ usaco/maxcross/maxcross.java
@@ -6,7 +6,7 @@ public class maxcross {
 								 		K, // Continugous block length
 										B; // Number of broken crosswalks
 
-	public static ArrayList<boolean[]> perms;
+	public static ArrayList<Perm> perms;
 
 
 	public static void main(String[] args) throws IOException {
@@ -30,28 +30,32 @@ public class maxcross {
 
 		Arrays.sort(broken);
 
-		perms = new ArrayList<boolean[]>();
+		perms = new ArrayList<Perm>();
 		dfs(broken, new boolean[B], 0);
-		for (int i = 0; i < perms.size() - 1; i++) {
-			boolean[] usedPerm = perms.get(i);
-			// For each permutation, make an array of those crosswalks and see
-			// if it satisfies the K sized block of crosswalks
+
+		// Sort the permutations, from least fixed to most fixed
+		Collections.sort(perms);
+
+		// For each permutation, make an array of those crosswalks and see
+		// if it satisfies the K sized block of crosswalks
+		for (int i = 0; i < perms.size(); i++) {
+			Perm p = perms.get(i);
 			int[] curCase = new int[N];
-			for (int j = 0; j < usedPerm.length; j++) {
-				if (usedPerm[j])
+			// Break the appropriate crosswalks
+			for (int j = 0; j < p.data.length; j++) {
+				if (p.data[j])
 				curCase[broken[j] - 1] = 1;
 			}
 
 			// For each of the successful blocks, see which one requires the least
 			// number of fixed
 			int range = findRange(curCase);
-			// if (range >= K)
-			// 	min = Math.min(min, ));
+			if (range >= K) {
+				min = Math.min(min, p.nFixed);
+				break;
+			}
 		}
-
-
 		pw.println(min);
-
 		pw.close();
 	}
 
@@ -71,8 +75,6 @@ public class maxcross {
 			else
 				counter++;
 		}
-
-
 		return max;
 	}
 
@@ -86,32 +88,43 @@ public class maxcross {
 				used[i] = false;
 			}
 		}
-		boolean[] tmp = new boolean[used.length];
-		for (int j = 0; j < used.length; j++) {
-			tmp[j] = used[j];
-		}
+		Perm tmp = new Perm(used);
 		perms.add(tmp);
 
 	}
-
-
-
 }
 
-
-class Perm {
-	boolean[] used;
+class Perm implements Comparable<Perm>{
+	boolean[] data;
 	int nFixed;
-	public Perm() {
+	public Perm(boolean[] u) {
+		boolean[] tmp = new boolean[u.length];
+		for (int i = 0; i < u.length; i++) {
+			tmp[i] = u[i];
+		}
+		this.data = tmp;
 
+		this.nFixed = this.numFixed();
 	}
 
-	private int numFixed(boolean[] a) {
+	private int numFixed() {
 		int counter = 0;
-		for (int i = 0; i < a.length; i++)
-			if (a[i])
+		for (int i = 0; i < this.data.length; i++)
+			if (!this.data[i])
 				counter++;
-
 		return counter;
+	}
+
+	public int compareTo(Perm other) {
+		if (this.nFixed < other.nFixed)
+			return 1;
+		else if (this.nFixed < other.nFixed)
+			return -1;
+		else
+			return 0;
+	}
+
+	public String toString() {
+		return Arrays.toString(data) + " --> " + nFixed;
 	}
 }
