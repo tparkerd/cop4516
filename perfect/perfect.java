ede3466a9252ba479dd7f5d8e6796407dd43bad2
Tim <tim.parkerd@gmail.com>
2017-02-19 16:12:43 -0500

    perfect finished

--- perfect/perfect.java
+++ perfect/perfect.java
@@ -4,68 +4,34 @@ public class perfect {
 	public static final boolean DEBUG = false;
 	public static void main(String args[]) {
 		Scanner stdin = new Scanner(System.in);
-			
+
 		// For each number (case)...
 		int nCases = stdin.nextInt();
 		for (int i = 0; i < nCases; i++) {
 			long cNum = stdin.nextLong(); // Next case value/number
-			long[] result = euclid(cNum, 0);
-			//System.out.println(Arrays.toString(result));
-			//System.out.println(gcd(cNum, cNum));
-			if (DEBUG) System.out.println(findFactors(cNum).toString());
-			System.out.println(solve(findFactors(cNum), cNum));
-		}
-	}
-	
-	// Not sure if it's extended euclid
-	public static long[] euclid(long a, long b) {
-		if (b == 0) {
-			if (DEBUG) System.out.printf("(%d, %d, %d)\n", 1, 0 , a);
-			return new long[] {1, 0, a};
-		}
-		
-		else {
-			long q = a / b;
-			long r = a % b;
-			long[] rec = euclid(b, r);
-			if (DEBUG) System.out.printf("(%d, %d, %d)\n", rec[1], rec[0] - q * rec[1] , rec[2]);
-			return new long[] {rec[1], rec[0] - q * rec[1], rec[2]};
+			System.out.println(solve(cNum));
 		}
 	}
 
-	
-	// Let's try GCD instead b/c that makes more sense at the moment
-	public static long gcd(long a, long b) {
-		if (b == 0)
-			if (DEBUG) System.out.println(a);
-		else
-			if (DEBUG) System.out.println(gcd(b, a % b));
-		return b == 0 ? a : gcd(b, a % b);
-	}
-	
-	// Let's brute force instead! I know this isn't the solution he wants, but I want to know if I actually understand
-	// How am I supposed to optimally get all the factors, not just the prime factors...blegh
-	public static ArrayList<Long> findFactors(Long a) {
-		ArrayList<Long> tmp = new ArrayList<Long>(); // Holds all the valid factors
-		for (long i = 2; i < a; i++) {
-			if (a % i == 0)
-				tmp.add(i);
+	public static String solve(long value) {
+		long runningTotal = 1;
+		for (long i = 2; i <= Math.sqrt(value); i++) {
+			if (value % i == 0) {
+				if (i == Math.sqrt(value))
+					runningTotal += i;
+				else
+					runningTotal += (i + (value / i));
+			}
 		}
-		return tmp;
-	}
-	
-	public static String solve(ArrayList<Long> a, Long v) {
-		long total = 0;
-		for (Long value : a) {
-			total += value;
-		}
-	
-		if (total > v)
-			return new String("abundant");
-		else if (total < v) {
+		if (DEBUG) System.out.println(runningTotal);
+
+		if (runningTotal == value)
+			return new String("perfect");
+		else if (runningTotal < value)
 			return new String("defective");
-		}
-		return new String("perfect");
+		else if (runningTotal > value)
+			return new String("abundant");
+		else
+			return new String("wrong answer");
 	}
-	
 }
