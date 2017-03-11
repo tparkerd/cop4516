2a102a09e591a7fa790af5fa3e6cc253f3b27d85
Tim <tim.parkerd@gmail.com>
2017-03-11 01:38:10 -0500

    functional and fast, but not the correct answer

--- alphabet/alphabet.java
+++ alphabet/alphabet.java
@@ -1,263 +1,158 @@
 import java.util.*;
 
 public class alphabet {
-  public static final boolean DEBUG = true;
+	public static int N = 52; // number of possible letters
+	public static final boolean DEBUG = false;
 
-	private Edge[][] adjMat;
-	private int source;
-	private int dest;
+	public static void main(String[] args) {
+		Scanner stdin = new Scanner(System.in);
+		int nCases = Integer.parseInt(stdin.nextLine());
 
+		// For each case...
+		for (int i = 0; i < nCases; i++) {
+			// Get all the letters and split into two
+			String[] letters = stdin.nextLine().split(" ");
+			char[] redLetters = letters[0].toCharArray();
+			char[] greenLetters = letters[1].toCharArray();
+			int[][] adjMatrix = new int[N][N];
 
-  public static void main(String[] args) {
-    Scanner stdin = new Scanner(System.in);
-    int nCases = Integer.parseInt(stdin.nextLine());
+			// Fill in actual connections between red and green letters
+			for (int r = 0; r < redLetters.length; r++) {
+				for (int g = 0; g < greenLetters.length; g++) {
+					if (Math.abs(redLetters[r] - greenLetters[g]) > 2) {
+						if (DEBUG) System.out.println(convert(greenLetters[g]) + " = " + (greenLetters[g] - 'a' + 'A'));
+						adjMatrix[redLetters[r] - 'a'][convert(greenLetters[g])] = 1;
+					}
+				}
+			}
 
-    // For each case...
-    for (int i = 0; i < nCases; i++) {
-      // Get all the letters and split into two
-      String[] letters = stdin.nextLine().split(" ");
-      char[] redLetters = letters[0].toCharArray();
-      char[] greenLetters = letters[1].toCharArray();
-      int[][] adjMatrix = new int[28][28];
+			if (DEBUG) System.out.println("-: [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z]");
 
-      // Fill in the connection for the letters
-      // Connect the source to the red letters
-      Arrays.fill(adjMatrix[26], 1);
-      // Disconnect source from sink directly
-      adjMatrix[26][27] = 0;
-      // Diconnect source from itself
-      adjMatrix[26][26] = 0;
-      // Connect all green letters to sink
-      for (int q = 0; q < 26; q++) {
-        adjMatrix[q][27] = 1;
-      }
-
-
-
-      // Fill in actual connections between red and green letters
-      for (int r = 0; r < redLetters.length; r++) {
-        for (int g = 0; g < greenLetters.length; g++) {
-          if (Math.abs(redLetters[r] - greenLetters[g]) > 2)
-            adjMatrix[redLetters[r] - 'a'][greenLetters[g] - 'a'] = 1;
-        }
-      }
-
-      if (DEBUG) System.out.println("-: [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z, >, *]");
-
-      // Debug, print the adjacency matrix
-      for (int q = 0; q < adjMatrix.length; q++) {
-        if (q == 26) {
-          if (DEBUG) System.out.println(">: " + Arrays.toString(adjMatrix[q]));
-        } else if (q == 27) {
-          if (DEBUG) System.out.println("*: " + Arrays.toString(adjMatrix[q]));
-        }
-        else {
-          if (DEBUG) System.out.println((char)(q + 'a') + ": " + Arrays.toString(adjMatrix[q]));
-        }
-      }
+			// Debug, print the adjacency matrix
+			for (int q = 0; q < adjMatrix.length - 2; q++) {
+					if (DEBUG) System.out.println((char)(q + 'a') + ": " + Arrays.toString(adjMatrix[q]));
+			}
+			FordFulkerson graph = new FordFulkerson(N);
 
 
-      networkflow graph = new networkflow(adjMatrix, 0, 27);
-      int ans = graph.getMaxFlow();
-      System.out.println(ans);
+			// Connect source and sink
+			// Connect source to all red letters
+			for (int q = 0; q < redLetters.length; q++) {
+				if (DEBUG) System.out.printf("Add source [%d][%c]\n", N, redLetters[q]);
+				graph.add(N, redLetters[q] - 'a', 1);
+			}
 
+			// Connect green letters to sink
+			for (int q = 0; q < greenLetters.length; q++) {
+				if (DEBUG) System.out.printf("Add sink: [%c][%d]\n", greenLetters[q], N + 1);
+				graph.add(convert(greenLetters[q]), N + 1, 1);
+			}
 
-    }
-  }
+			// Add edges to graph
+			for (int row = 0; row < N; row++) {
+				for (int col = 0; col < N; col++) {
+					if (adjMatrix[row][col] > 0) {
+						if (DEBUG) System.out.printf("Adding matrix[%c][%c]\n", row + 'a', col + 'a');
+						graph.add(row, col, 1);
+					}
+				}
+			}
+
+			int ans = graph.ff();
+			System.out.println(ans);
+		}
+	}
+
+	public static int convert(char c) {
+			return (c - 'a' + 26);
+	}
 }
 
 
+class FordFulkerson {
 
+	// Stores graph.
+	public int[][] cap;
+	public int n;
+	public int source;
+	public int sink;
 
+	// "Infinite" flow.
+	final public static int oo = (int)(1E9);
 
+	// Set up default flow network with size+2 vertices, size is source, size+1 is sink.
+	public FordFulkerson(int size) {
+		n = size + 2;
+		source = n - 2;
+		sink = n - 1;
+		cap = new int[n][n];
+	}
 
+	// Adds an edge from v1 -> v2 with capacity c.
+	public void add(int v1, int v2, int c) {
+		cap[v1][v2] = c;
+	}
 
+	// Wrapper function for Ford-Fulkerson Algorithm
+	public int ff() {
 
+		// Set visited array and flow.
+		boolean[] visited = new boolean[n];
+		int flow = 0;
 
-  class networkflow {
+		// Loop until no augmenting paths found.
+		while (true) {
 
-  	private Edge[][] adjMat;
-  	private int source;
-  	private int dest;
+			// Run one DFS.
+			Arrays.fill(visited, false);
+			int res = dfs(source, visited, oo);
 
-  	// All positive entries in flows should represent valid flows
-  	// between vertices. All other entries must be 0 or negative.
-  	public networkflow(int[][] flows, int start, int end) {
+			// Nothing found, get out.
+			if (res == 0) break;
 
-  		source = start;
-  		dest = end;
-  		adjMat = new Edge[flows.length][flows.length];
+			// Add this flow.
+			flow += res;
+		}
 
-  		for (int i=0; i<flows.length; i++) {
-  			for (int j=0; j<flows[i].length; j++) {
+		// Return it.
+		return flow;
+	}
 
-  				// Fill in this flow.
-  				if (flows[i][j] > 0)
-  					adjMat[i][j] = new Edge(flows[i][j]);
-  				else
-  					adjMat[i][j] = null;
-  			}
-  		}
-  	}
+	// DFS to find augmenting math from v with maxflow at most min.
+	public int dfs(int v, boolean[] visited, int min) {
 
-  	public ArrayList<direction> findAugmentingPath() {
+		// got to the sink, this is our flow.
+		if (v == sink)  return min;
 
-  		// This will store the previous node visited in the BFS.
-  		direction[] prev = new direction[adjMat.length];
-  		boolean[] inQueue = new boolean[adjMat.length];
-  		for (int i=0; i<inQueue.length; i++)
-  			inQueue[i] = false;
+		// We've been here before - no flow.
+		if (visited[v])  return 0;
 
-  		// The source has no previous node.
-  		prev[source] = new direction(-1, true);
+		// Mark this node and recurse.
+		visited[v] = true;
+		int flow = 0;
 
-  		LinkedList<Integer> bfs_queue = new LinkedList<Integer>();
-  		bfs_queue.offer(new Integer(source));
-  		inQueue[source] = true;
+		// Just loop through all possible next nodes.
+		for (int i = 0; i < n; i++) {
 
-  		// Our BFS will go until we clear out the queue.
-  		while (bfs_queue.size() > 0) {
+			// We can augment in this direction.
+			if (cap[v][i] > 0)
+				flow = dfs(i, visited, Math.min(cap[v][i], min));
 
-  			// Add all the new neighbors of the current node.
-  			Integer next = bfs_queue.poll();
+			// We got positive flow on this recursive route, return it.
+			if (flow > 0) {
 
-  			// Find all neighbors and add into the queue. These are forward edges.
-  			for (int i=0; i<adjMat.length; i++)
-  				if (!inQueue[i] && adjMat[next][i] != null && adjMat[next][i].maxPushForward() > 0) {
-  					bfs_queue.offer(new Integer(i));
-  					inQueue[i] = true;
-  					prev[i] = new direction(next, true);
-  				}
+				// Subtract it going forward.
+				cap[v][i] -= flow;
 
-  			// Now look for back edges.
-  			for (int i=0; i<adjMat.length; i++)
-  				if (!inQueue[i] && adjMat[i][next] != null && adjMat[i][next].maxPushBackward() > 0) {
-  					bfs_queue.offer(new Integer(i));
-  					inQueue[i] = true;
-  					prev[i] = new direction(next, false);
-  				}
-  		}
+				// Add it going backwards, so that later, we can flow back through this edge as a backedge.
+				cap[i][v] += flow;
 
-  		// No augmenting path found.
-  		if (!inQueue[dest])
-  			return null;
+				// Return this flow.
+				return flow;
+			}
+		}
 
-  		ArrayList<direction> path = new ArrayList<direction>();
-
-  		direction place = prev[dest];
-
-  		direction dummy = new direction(dest, true);
-  		path.add(dummy);
-
-  		// Build the path backwards.
-  		while (place.prev != -1) {
-  			path.add(place);
-  			place = prev[place.prev];
-  		}
-
-  		// Reverse it now.
-  		Collections.reverse(path);
-
-  		return path;
-  	}
-
-  	// Run the Max Flow Algorithm here.
-  	public int getMaxFlow() {
-
-  		int flow = 0;
-
-  		ArrayList<direction> nextpath = findAugmentingPath();
-
-  		// Loop until there are no more augmenting paths.
-  		while (nextpath != null) {
-
-  			// Check what the best flow through this path is.
-  			int this_flow = Integer.MAX_VALUE;
-  			for (int i=0; i<nextpath.size()-1; i++) {
-
-  				if (nextpath.get(i).forward) {
-  					this_flow = Math.min(this_flow, adjMat[nextpath.get(i).prev][nextpath.get(i+1).prev].maxPushForward());
-  				}
-  				else {
-  					this_flow = Math.min(this_flow, adjMat[nextpath.get(i+1).prev][nextpath.get(i).prev].maxPushBackward());
-  				}
-  			}
-
-  			// Now, put this flow through.
-  			for (int i=0; i<nextpath.size()-1; i++) {
-
-  				if (nextpath.get(i).forward) {
-  					adjMat[nextpath.get(i).prev][nextpath.get(i+1).prev].pushForward(this_flow);
-  				}
-  				else {
-  					adjMat[nextpath.get(i+1).prev][nextpath.get(i).prev].pushBack(this_flow);
-  				}
-  			}
-
-  			// Add this flow in and then get the next path.
-  			flow += this_flow;
-  			nextpath = findAugmentingPath();
-
-  		}
-
-  		return flow;
-  	}
-  }
-
-  class Edge {
-
-  	private int capacity;
-  	private int flow;
-
-  	public Edge(int cap) {
-  		capacity = cap;
-  		flow = 0;
-  	}
-
-  	public int maxPushForward() {
-  		return capacity - flow;
-  	}
-
-  	public int maxPushBackward() {
-  		return flow;
-  	}
-
-  	public boolean pushForward(int moreflow) {
-
-  		// We can't push through this much flow.
-  		if (flow+moreflow > capacity)
-  			return false;
-
-  		// Push through.
-  		flow += moreflow;
-  		return true;
-  	}
-
-  	public boolean pushBack(int lessflow) {
-
-  		// Not enough to push back on.
-  		if (flow < lessflow)
-  			return false;
-
-  		flow -= lessflow;
-  		return true;
-  	}
-  }
-
-  class direction {
-
-  	public int prev;
-  	public boolean forward;
-
-  	public direction(int node, boolean dir) {
-  		prev = node;
-  		forward = dir;
-  	}
-
-  	public String toString() {
-  		if (forward)
-  			return "" + prev + "->";
-  		else
-  			return "" + prev + "<-";
-  	}
-  }
+		// If we get here there was no flow.
+		return 0;
+	}
+}
