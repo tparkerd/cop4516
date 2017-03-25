8c69bc389a0eeb273736f7aa8be7736fb0194e71
Tim P <Tim.ParkerD@gmail.com>
2017-03-25 04:13:16 +0000

    correct answers, but super slow

--- lotto/lotto.java
+++ lotto/lotto.java
@@ -1,7 +1,63 @@
 import java.util.*;
 
 public class lotto {
-  public static void main(String[] args) {
+  public static final boolean DEBUG = false;
+  public static int counter;
 
+  public static void main(String[] args) {
+    Scanner stdin = new Scanner(System.in);
+
+    // // Row is the starting number, column is the upper limit value
+    // int[][] solutions = new int[11][20];
+    // solutions[0][0] = 1;
+    // for (int i = 0; i < solutions.length; i++) {
+    //   if (DEBUG) System.out.println(Arrays.toString(solutions[i]));
+    // }
+
+
+
+    // Get the first case
+    int n = stdin.nextInt();
+    int m = stdin.nextInt();
+    counter = 0;
+
+    // For each test case...
+    int cCase = 1;
+    while (n != 0 && m != 0) {
+      boolean[] used = new boolean[m];
+      Arrays.fill(used, false);
+      // if (DEBUG) System.out.println(Arrays.toString(used));
+      odometer(new int[n], m, 0);
+      System.out.printf("Case %d: n = %d, m = %d, # lists = %d\n", cCase++, n, m, counter);
+
+      // Get next case.
+      n = stdin.nextInt();
+      m = stdin.nextInt();
+      counter = 0;
+    }
+  }
+
+  public static void odometer(int[] digits, int numDigits, int k) {
+    // Make sure the sub-permutation is valid first
+    for (int i = 0; i < k - 1; i++) {
+      if ((digits[i] * 2) > digits[i + 1]) {
+        // System.out.println("ERROR, NOT CORRECT PERM");
+        return;
+      }
+    }
+
+    if (k == digits.length) {
+      // System.out.printf("Call: odometer(%s, %d, %d)\n", Arrays.toString(digits), numDigits, k);
+      // System.out.println("Test for valid perm");
+      // See if each each value is twice or more than the previous one
+
+      counter++;
+      if (DEBUG) System.out.println(Arrays.toString(digits));
+      return;
+    }
+    for (int i = 1; i <= numDigits; i++) {
+      digits[k] = i;
+      odometer(digits, numDigits, k + 1);
+    }
   }
 }
