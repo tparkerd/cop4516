f6c0cbeca696bf6740720467cc2b8960eb537031
Tim <tim.parkerd@gmail.com>
2017-03-26 00:47:08 -0400

    Precomputed distances, but moving towards distances calculated on the fly

--- robot/robot.java
+++ robot/robot.java
@@ -1,92 +1,166 @@
-import java.util.ArrayList;
-import java.util.Scanner;
-
-class pair{
-	int x;
-	int y;
-	int penalty;
-	double time;
-	
-	public pair(int a, int b, int p){
-		this.x = a;
-		this.y = b;
-		this.penalty = p;
-		time = 0;
-	}
-	
-}
+import java.util.*;
+import java.math.*;
+import java.text.*;
 
 public class robot {
+    static final boolean DEBUG = true;
+    static final int oo = (int) 1e9;
+    static int n;
+    static ArrayList<Edge>[] g;
 
-	public static void main(String args[]){
-		Scanner R = new Scanner(System.in);
-		int n;
-		ArrayList<pair> Targets;
-		pair temp;
-		int x, y, z;
-		int Total;
-		while(true){
-			n = R.nextInt();
-			if(n == 0){
-				return;
-			}
-			Targets = new ArrayList<>();
-			Total = 0;
-			for(int i = 0; i < n; i++){
-				x = R.nextInt();
-				y=R.nextInt();
-				z=R.nextInt();
-				Total = Total+z;
-				temp = new pair(x,y,z);
-				Targets.add(temp);
-			}
-			Calculate(Targets, Total);
-		}
-	}
-	
-	
-	
-	public static void Calculate(ArrayList<pair> Targets, int TotalPenalty){
-		pair start = new pair(0,0,0);
-		pair end = new pair(100,100,0);
-		
-		double TimeTo = distance(start, Targets.get(0))+1;
-		double distance1;
-		double distance2;
-		
-		while(Targets.size() > 1){
-			distance1 = TimeTo + distance(Targets.get(0), Targets.get(1))+1;		
-			distance2 = distance(start, Targets.get(1)) + Targets.get(0).penalty+1;
-			
-			
-			if(distance1 > distance2){
-				TimeTo = distance2;
-			} else{
-				TimeTo = distance1;
-			}
-			Targets.remove(0);
-		}
+  @SuppressWarnings("unchecked")
+  public static void main(String[] args) {
 
-		distance1 = TimeTo + distance(Targets.get(0), end)+1;
-		distance2 = TotalPenalty+distance(start,end)+1;
-		
-		if(distance1 > distance2){
-			System.out.printf("%.3f\n", distance2);
-		} else{
-			System.out.printf("%.3f\n", distance1);
-		}
-		
-	}
-	
-	public static double distance(pair One, pair Two){
-		if(One == Two){
-			return 0;
-		}
-		
-		int Newx = One.x - Two.x;
-		int Newy = One.y - Two.y;
-		double out = Math.sqrt(  (Newx*Newx)+(Newy*Newy)  );
-		return out;
-	}
-	
+    int q = 101;
+
+    // Create a big ole box to store all the possible distances
+    // Each index is the value of either the X or Y position on an
+    // everyday Cartesian grid
+    double[][][][] d = new double[q][q][q][q];
+    for (double[][][] a : d)
+      for (double[][] b : a)
+        for (double[] c : b)
+          Arrays.fill(c, 0);
+
+    // Precompute all the distances of the possible points
+    // NOTE(timp): this could probably be optimized by storing this in a
+    // 2-D array of just the differences between point A and point B
+    // So the points (2, 2) -> (5, 6) and (3, 3) -> (6, 7) would both
+    // just store their distances in the position (3, 4).
+    for (int x1 = 0; x1 < q; x1++)
+      for (int y1 = 0; y1 < q; y1++)
+        for (int x2 = 0; x2 < q; x2++)
+          for (int y2 = 0; y2 < q; y2++) {
+            int xDist = Math.abs(x2 - x1);
+            int yDist = Math.abs(y2 - y1);
+            // BigDecimal = new BigDecimal(Math.sqrt( (xDist * xDist) + (yDist * yDist) ))
+            // .setScale(2, RoundingMode.HALF_EVEN);
+            d[x1][y1][x2][y2] = Math.sqrt( (xDist * xDist) + (yDist * yDist) );
+          }
+
+    // Yep! It totally works that way, and you can precompute and store
+    // it faster this way
+    double grid[][] = new double[q][q];
+    for (int xdist = 0; xdist < q; xdist++) {
+      for (int ydist = 0; ydist < q; ydist++) {
+        grid[xdist][ydist] = Math.sqrt( (xdist * xdist) + (ydist * ydist) );
+      }
+    }
+
+
+    // Get the distance from point (x1, y1) and (x2, y2);
+    if (DEBUG) System.out.println(d[3][20][10][62]);
+    if (DEBUG) System.out.println(d[0][0][100][100] + 20);
+    // First point S, (0,0) is always 1 second in order to turn, and then
+    // Another second for every node hit, but the end does not add one b/c it
+    // does not need to turn to face another node
+    if (DEBUG) System.out.println(d[0][0][50][50] + d[50][50][100][100] + 1 + 1);
+    // God f'ing damn it, finally it lets me keep my damn trailing zeros!
+    if (DEBUG) System.out.printf("%s\n", (new DecimalFormat("#.000").format(grid[7][42])));
+
+
+    if (DEBUG) System.out.println((new Target(0,0,0,0)).getDistanceTo(new Target(1,100,100,0)));
+
+
+
+      // Scanner stdin = new Scanner(System.in);
+      // n = stdin.nextInt();
+      //
+      // while (n != 0) {
+      //   if (DEBUG) System.out.println("N: " + n);
+      //   g = new ArrayList[101];
+      //   for (ArrayList list : g) list = new ArrayList<Integer>();
+      //   // Start is always 0,0 and finish at 100, 100
+      //   g[0].add(new Edge(0, 0));   // Add the starting point
+      //   g[0].add(new Edge(100, 100)); // Add the final target, takes 100s to get there
+      //
+      //   // For each target, add it to the graph
+      //   for (int i = 0; i < n; i++) {
+      //
+      //   }
+      //
+      //   // Next case
+      //   n = stdin.nextInt();
+      // }
+  }
+
+
+
+
+
+
+
+
+
+
+
+  // Finds shortest distance from s to d
+  public static double dijkstras(int s, int d) {
+
+    // Set up the priority queue.
+    boolean[] visited = new boolean[n];
+    PriorityQueue<Edge> pq = new PriorityQueue<Edge>();
+    pq.add(new Edge(s, 0));
+
+    // Go till empty.
+    while (!pq.isEmpty()) {
+
+      // Get the next edge.
+      Edge at = pq.poll();
+      if (visited[at.e]) continue;
+      visited[at.e] = true;
+
+      // We made it, return the distance.
+      if (at.e == d) return at.w;
+
+      // Enqueue all the neighboring edges.
+      for (Edge adj : g[at.e])
+        if (!visited[adj.e]) pq.add(new Edge(adj.e, adj.w + at.w));
+    }
+    return oo;
+  }
+
+  public static double findDistance(int a, int b) {
+    return Math.sqrt((a*a) + (b*b));
+  }
+}
+
+// Stores where an edge is going to and its weight.
+class Edge implements Comparable<Edge> {
+  int e;
+  double w;
+
+  public Edge(int e, double w) {
+    this.e = e;
+    this.w = w;
+  }
+
+  public int compareTo(Edge o) {
+    if (w > o.w)
+      return 1;
+    else if( w < o.w)
+      return -1;
+    else
+      return 0;
+  }
+}
+
+class Target {
+  int id;
+  int x;
+  int y;
+  int pen;
+
+  public Target(int id, int x, int y, int pen) {
+    this.id = id;
+    this.x = x;
+    this.y = y;
+    this.pen = pen;
+  }
+
+  public double getDistanceTo(Target next) {
+    int xdist = Math.abs(this.x - next.x);
+    int ydist = Math.abs(this.y - next.y);
+    return Math.sqrt( (xdist * xdist) + (ydist * ydist) );
+  }
 }
