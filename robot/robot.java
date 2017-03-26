930317fa1ba54be20bf232811436a23131fc3804
Tim <tim.parkerd@gmail.com>
2017-03-26 02:08:41 -0400

    solution candidate

--- robot/robot.java
+++ robot/robot.java
@@ -3,102 +3,86 @@ import java.math.*;
 import java.text.*;
 
 public class robot {
-    static final boolean DEBUG = true;
-    static final int oo = (int) 1e9;
-    static int n;
-    static ArrayList<Edge>[] g;
+  static final boolean DEBUG = false;
+  static final int oo = (int) 1e9;
+  static final int N = 101;
+  public static ArrayList<Edge>[] g;
 
   @SuppressWarnings("unchecked")
   public static void main(String[] args) {
+    Scanner stdin = new Scanner(System.in);
+    int n = stdin.nextInt();
 
-    int q = 101;
-
-    // Create a big ole box to store all the possible distances
-    // Each index is the value of either the X or Y position on an
-    // everyday Cartesian grid
-    double[][][][] d = new double[q][q][q][q];
-    for (double[][][] a : d)
-      for (double[][] b : a)
-        for (double[] c : b)
-          Arrays.fill(c, 0);
-
-    // Precompute all the distances of the possible points
-    // NOTE(timp): this could probably be optimized by storing this in a
-    // 2-D array of just the differences between point A and point B
-    // So the points (2, 2) -> (5, 6) and (3, 3) -> (6, 7) would both
-    // just store their distances in the position (3, 4).
-    for (int x1 = 0; x1 < q; x1++)
-      for (int y1 = 0; y1 < q; y1++)
-        for (int x2 = 0; x2 < q; x2++)
-          for (int y2 = 0; y2 < q; y2++) {
-            int xDist = Math.abs(x2 - x1);
-            int yDist = Math.abs(y2 - y1);
-            // BigDecimal = new BigDecimal(Math.sqrt( (xDist * xDist) + (yDist * yDist) ))
-            // .setScale(2, RoundingMode.HALF_EVEN);
-            d[x1][y1][x2][y2] = Math.sqrt( (xDist * xDist) + (yDist * yDist) );
-          }
-
-    // Yep! It totally works that way, and you can precompute and store
-    // it faster this way
-    double grid[][] = new double[q][q];
-    for (int xdist = 0; xdist < q; xdist++) {
-      for (int ydist = 0; ydist < q; ydist++) {
-        grid[xdist][ydist] = Math.sqrt( (xdist * xdist) + (ydist * ydist) );
+    while (n != 0) {
+      if (DEBUG) System.out.println("N: " + n);
+      ArrayList<Target> list = new ArrayList<Target>();
+      // Add the starting node
+      list.add(new Target(0, 0, 0, 1));
+      // Remember all of the target on the grid, as any target connects to
+      // any target that follows it.
+      for (int i = 1; i <= n; i++) {
+        int x = stdin.nextInt();
+        int y = stdin.nextInt();
+        int pen = stdin.nextInt();
+        if (DEBUG) System.out.printf("Add Target(%d, %d, %d, %d)\n", i, x, y, pen);
+        list.add(new Target(i, x, y, pen));
       }
+      // Add the final target
+      if (DEBUG) System.out.println("List size: " + list.size());
+      list.add(new Target(list.size(), 100, 100, 0));
+
+      // DEBUG
+      if (DEBUG) System.out.println("Show list");
+      for (Target t : list)
+        if (DEBUG) System.out.println(t);
+
+      // Create graph
+      g = new ArrayList[101];
+      for (int i = 0; i < g.length; i++)
+        g[i] = new ArrayList<Edge>();
+
+      for (int i = 0; i < list.size(); i++) {
+        // Keep track of penalty points for skipped targets and reset it
+        // when you test from a later starting node
+        int penaltyRunningTotal = 0;
+        // Connect the current target to all that follow it
+        // Making sure to update the penalty for each of the nodes skipped
+        for (int j = i + 1; j < list.size(); j++) {
+          // Add to graph
+          // Overall weight is the...
+          // Distance between last target and this one, sum of all previous
+          // penalty points, and one second to turn towards the next target
+          int edge = list.get(j).id;
+          double distToNextTarget = list.get(i).getDistanceTo(list.get(j));
+          if (DEBUG) System.out.printf("g[%d].add(new Edge(%d, %.3f + 1 + %d = %.3f));\n", i, edge, distToNextTarget, penaltyRunningTotal, distToNextTarget + 1 + penaltyRunningTotal);
+          g[i].add(new Edge(edge, distToNextTarget + 1 + penaltyRunningTotal));
+          penaltyRunningTotal += list.get(j).pen;
+        }
+      }
+
+      // DEBUG: show graph
+      if (DEBUG) System.out.println("Show graph");
+      for (ArrayList<Edge> l : g)
+        if (DEBUG) System.out.println(l.toString());
+
+
+      // Answer!
+      // God f'ing damn it, finally it lets me keep my damn trailing zeros!
+      if (DEBUG) System.out.println(N - 1);
+      double answer = dijkstras(0, list.size() - 1);
+      System.out.printf("%s\n", (new DecimalFormat("#.000").format(answer)));
+
+
+      // Next case
+      n = stdin.nextInt();
     }
-
-
-    // Get the distance from point (x1, y1) and (x2, y2);
-    if (DEBUG) System.out.println(d[3][20][10][62]);
-    if (DEBUG) System.out.println(d[0][0][100][100] + 20);
-    // First point S, (0,0) is always 1 second in order to turn, and then
-    // Another second for every node hit, but the end does not add one b/c it
-    // does not need to turn to face another node
-    if (DEBUG) System.out.println(d[0][0][50][50] + d[50][50][100][100] + 1 + 1);
-    // God f'ing damn it, finally it lets me keep my damn trailing zeros!
-    if (DEBUG) System.out.printf("%s\n", (new DecimalFormat("#.000").format(grid[7][42])));
-
-
-    if (DEBUG) System.out.println((new Target(0,0,0,0)).getDistanceTo(new Target(1,100,100,0)));
-
-
-
-      // Scanner stdin = new Scanner(System.in);
-      // n = stdin.nextInt();
-      //
-      // while (n != 0) {
-      //   if (DEBUG) System.out.println("N: " + n);
-      //   g = new ArrayList[101];
-      //   for (ArrayList list : g) list = new ArrayList<Integer>();
-      //   // Start is always 0,0 and finish at 100, 100
-      //   g[0].add(new Edge(0, 0));   // Add the starting point
-      //   g[0].add(new Edge(100, 100)); // Add the final target, takes 100s to get there
-      //
-      //   // For each target, add it to the graph
-      //   for (int i = 0; i < n; i++) {
-      //
-      //   }
-      //
-      //   // Next case
-      //   n = stdin.nextInt();
-      // }
   }
-
-
-
-
-
-
-
-
-
-
 
   // Finds shortest distance from s to d
   public static double dijkstras(int s, int d) {
 
     // Set up the priority queue.
-    boolean[] visited = new boolean[n];
+    boolean[] visited = new boolean[N];
     PriorityQueue<Edge> pq = new PriorityQueue<Edge>();
     pq.add(new Edge(s, 0));
 
@@ -107,15 +91,22 @@ public class robot {
 
       // Get the next edge.
       Edge at = pq.poll();
+      if (DEBUG) System.out.println("EDGE ID: " + at.e + " D: " + d);
       if (visited[at.e]) continue;
       visited[at.e] = true;
 
       // We made it, return the distance.
-      if (at.e == d) return at.w;
+      if (at.e == d) {
+        if (DEBUG) System.out.println("Solution found " + at.w);
+        return at.w;
+      }
 
       // Enqueue all the neighboring edges.
       for (Edge adj : g[at.e])
-        if (!visited[adj.e]) pq.add(new Edge(adj.e, adj.w + at.w));
+        if (!visited[adj.e]) {
+          pq.add(new Edge(adj.e, adj.w + at.w));
+          if (DEBUG) System.out.printf("pq.add(new Edge(%d, %.3f + %.3f));\n", adj.e, adj.w, at.w);
+        }
     }
     return oo;
   }
@@ -143,6 +134,10 @@ class Edge implements Comparable<Edge> {
     else
       return 0;
   }
+
+  public String toString() {
+    return "("+e+", "+w+")";
+  }
 }
 
 class Target {
@@ -162,5 +157,9 @@ class Target {
     int xdist = Math.abs(this.x - next.x);
     int ydist = Math.abs(this.y - next.y);
     return Math.sqrt( (xdist * xdist) + (ydist * ydist) );
+  }
+
+  public String toString() {
+    return "::"+id+" ("+x+", "+y+") " + pen;
   }
 }
