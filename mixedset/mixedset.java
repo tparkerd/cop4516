d0b9a6deed892b67eb1ed5a668edaf67e39a214e
Tim <tim.parkerd@gmail.com>
2017-03-31 17:19:07 -0400

    update before meeting

--- mixedset/mixedset.java
+++ mixedset/mixedset.java
@@ -30,117 +30,39 @@ public class mixedset {
 
   // Prints all possible seetings of odometer with n digits with the first k fixed.
   public static void printOdometer(int[] odometer, int k) {
-    if (DEBUG) System.out.printf("printOdometer(%s, %d);\n", Arrays.toString(odometer), k);
       // Base case.
-      if (k == odometer.length) process(odometer);
+      if (k == odometer.length) {
+        validate(odometer);
+        return;
+      }
 
       // Fill in each possible digit in slot k and recurse.
       else {
           int i;
-          for (i=0; i<=N; i++) {
+          for (i=0; i <= N; i++) {
               odometer[k] = i;
               printOdometer(odometer, k+1);
           }
       }
   }
 
-
-  public static void bar(int[] perm, boolean[] used, int k) {
-    System.out.printf("bar(%s, %s, %d);\n", Arrays.toString(perm), Arrays.toString(used), k);
-    if (k == perm.length) process(perm);
-
-    for (int i = 0; i < used.length; i++) {
-      if (!used[i]) {
-        used[i] = true;
-        perm[k] = i;
-        bar(perm, used, k + 1);
-        used[i] = false;
-      }
-    }
-  }
-
-  public static void process(int[] perm) {
-    System.out.println(Arrays.toString(perm));
-
-  }
-
-  public static void foo(int[] combo, boolean[] used, boolean[] diff, int k) {
-    if (DEBUG) System.out.printf("foo(%s, %s, %s, %d)\n", Arrays.toString(combo), Arrays.toString(used), Arrays.toString(diff), k);
-    // Found a valid combo
-    if (combo.length == k) {
-      System.out.println(":: " + Arrays.toString(combo));
-      return;
-    }
-    // For each of the possible values that could be placed
-    for (int i = 1; i < used.length; i++) {
-      // Get the last value
-      // If it's an empty list, this one will be valid regardless
-      if (k == 0) {
-        combo[k] = i; // Set the starting digit to the ith value
-        used[i] = true;
-        diff[0] = true;
-        // No need to check difference b/c it's just a single value
-        foo(combo, used, diff, k + 1);
-        used[i] = false;
-
-      // Otherwise, at least one digit was added previously, so we need
-      // to find out what would be the next greater value to add to the end
-      } else {
-        // From the largest number, N, get the next valid value to add to the combo
-        int big = used.length - 1;
-        if (DEBUG) System.out.println("Big index: " + big);
-        while (big > 1) {
-          if (used[big - 1]) break;
-          big--;
-        }
-        if (DEBUG) System.out.println("Big index'': " + big);
-
-        // Check to see if the differences haven't been already used
-        for (int d = 0; d < k; d++) {
-          if (DEBUG) System.out.printf("Comparing: |%d - %d| = %d\n", big, combo[d], Math.abs(big - combo[d]));
-          // Difference between candidate and last added item
-          int tmp = Math.abs(big - combo[d]);
-          if (!diff[tmp]) {
-            diff[tmp] = true;
-          } else {
-            return; // Oops, we already used this difference, forget this.
-          } // End difference check
-        } // End find next largest candidate
-        combo[k] = big;
-        used[big] = true;
-        foo(combo, used, diff, k + 1);
-        used[big] = false;
-        // Reset diff array
-        Arrays.fill(diff, false);
-        for (int d = 0; d < k; d++) {
-          for (int d2 = d + 1; d2 < k; d2++) {
-            diff[Math.abs(combo[d] - combo[d2])] = true;
-          }
-        }
-      }
-    }
-  }
-
-  public static void printcombosoriginal(int[] combo, int k) {
-    System.out.println("::" + Arrays.toString(combo));
-
-    int start = 0;
-    if (k > 0) start = combo[k - 1] + 1;
-
-    for (int i = start; i < combo.length; i++) {
-      combo[k] = i;
-      printcombosoriginal(combo, k + 1);
-    }
-  }
-
-
-  public static void validate(Integer[] perm) {
-    boolean[] used = new boolean[K];
+  public static void validate(int[] perm) {
+    boolean[] used = new boolean[K + 1];
     for (int i = 0; i < perm.length - 1; i++) {
-      int diff = Math.abs(perm[i] - perm[i + 1]);
-      if (used[diff]) return;
-      used[diff] = true;
+      for (int j = i + 1; j < perm.length; j++) {
+        // Base case: cannot start with zero
+        if (perm[i] == 0 || perm[j] == 0) return;
+        // Base case: values much be unique
+        if (perm[i] == perm[j]) return;
+        // Check that differences do not overlap
+        int diff = Math.abs(perm[i] - perm[j]);
+        if (used[diff])
+          return;
+        else
+          used[diff] = true;
+      }
     }
-    list.add(perm);
+    System.out.println("Passed: " + Arrays.toString(perm));
+    // list.add(perm);
   }
 }
