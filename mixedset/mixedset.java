f64fe64ed99cc6562fe03ac0a337af6807fd0e71
Tim <tim.parkerd@gmail.com>
2017-04-01 23:40:59 -0400

    candidate solution, removed the hashset check

--- mixedset/mixedset.java
+++ mixedset/mixedset.java
@@ -18,26 +18,23 @@ public class mixedset {
       l = new ArrayList<String>();
       N = stdin.nextInt();
       S = stdin.nextInt();
-      K = stdin.nextInt();
-      boolean[] differences = new boolean[N + 1];
-      differences[0] = true;
-      perm(new TreeSet<Integer>(), differences);
+      K = stdin.nextInt();;
+      perm(new TreeSet<Integer>());
       for (String s : l) {
         System.out.println(s);
       }
-      // if (DEBUG) System.out.println(l.get(K - 1));
+      if (DEBUG) System.out.println(l.get(K - 1));
 
     }
   }
 
   @SuppressWarnings("unchecked")
-  public static void perm(SortedSet set, boolean[] used) {
+  public static void perm(SortedSet set) {
     if (l.size() == K) return;
     if (set.size() >= S) {
-      if (h.contains(set)) return;
-      // if (DEBUG) System.out.println("::::"+set);
-      l.add(set.toString().replace("[", "").replace("]", "").replace(",", ""));
-      h.add(set);
+        // if (DEBUG) System.out.println("::::"+set);
+        l.add(set.toString().replace("[", "").replace("]", "").replace(",", ""));
+        h.add(set);
       return;
     } else {
       for (int i = 1; i <= N; i++) {
@@ -49,15 +46,15 @@ public class mixedset {
           if (DEBUG) System.out.println("Adding: " + i);
           set.add(i);
 
-          boolean[] tmpUsed = used.clone();
           // TODO(timp): Check that no two pairings share a difference
           // if not valid, return to back out of this branch
-          if (!checkDifferences(set, tmpUsed, i)) {
+          // if (!checkDifferences(set, tmpUsed, i)) {
+          if (!valid(set)) {
             set.remove(i);
             continue;
           }
           // Valid candidate set found
-          perm(set, tmpUsed.clone());
+          perm(set);
           set.remove(i);
         }
       }
@@ -65,26 +62,50 @@ public class mixedset {
   }
 
   @SuppressWarnings("unchecked")
-  public static boolean checkDifferences(SortedSet set, boolean[] used, int newValue) {
-    if (DEBUG) System.out.println("Call:" + set.toString());
-    if (DEBUG) System.out.println("Used: " + Arrays.toString(used));
-    // Single item, always just a difference of zero, so it's good, go back
+  public static boolean valid(SortedSet set) {
+    if (DEBUG) System.out.printf("Call: %s\n", set.toString());
     if (set.size() < 2) return true;
 
-    // Get all the values
-    ArrayList<Integer> values = new ArrayList(set);
+    ArrayList<Integer> s = new ArrayList<Integer>(set);
 
-    // From first (0th) to last (set.size() - 1) -- b/c I already added the item
-    for (int i = 0; i < set.size() - 1; i++) {
-      // See the difference between each item and the new one
-      int diff = Math.abs(values.get(i) - newValue);
-      if (DEBUG) System.out.printf("|%d - %d| = %d\n", values.get(i), newValue, diff);
-      if (used[diff]) {
-        if (DEBUG) System.out.println("FAIL");
-        return false;
+    int[] freq = new int[N + 1];
+    for (int i = 0; i < s.size(); i++) {
+      for (int j = i + 1; j < s.size(); j++) {
+        int diff = Math.abs(s.get(i) - s.get(j));
+        freq[diff]++;
+        if (freq[diff] > 1) {
+          if (DEBUG) System.out.println(Arrays.toString(freq));
+          if (DEBUG) System.out.println("Failed");
+          return false;
+        }
       }
-      used[diff] = true;
     }
+    if (DEBUG) System.out.println("Passed");
     return true;
   }
+
+  // @SuppressWarnings("unchecked")
+  // public static boolean checkDifferences(SortedSet set, boolean[] used, int newValue) {
+  //   if (DEBUG) System.out.println("Call:" + set.toString());
+  //   if (DEBUG) System.out.println("Used: " + Arrays.toString(used));
+  //   // Single item, always just a difference of zero, so it's good, go back
+  //   if (set.size() < 2) return true;
+  //
+  //   // Get all the values
+  //   ArrayList<Integer> values = new ArrayList(set);
+  //
+  //   // From first (0th) to last (set.size() - 1) -- b/c I already added the item
+  //   for (int i = 0; i < set.size() - 1; i++) {
+  //     // See the difference between each item and the new one
+  //     int diff = Math.abs(values.get(i) - newValue);
+  //     if (DEBUG) System.out.printf("|%d - %d| = %d\n", values.get(i), newValue, diff);
+  //     if (used[diff]) {
+  //       if (DEBUG) System.out.println("FAIL");
+  //       return false;
+  //     }
+  //     used[diff] = true;
+  //   }
+  //   return true;
+  // }
+
 }
