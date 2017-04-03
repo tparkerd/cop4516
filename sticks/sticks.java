a7aaaf1f25f7b314c99826ccb3ca7af364a82b03
Tim <tim.parkerd@gmail.com>
2017-04-03 17:14:26 -0400

    update before class

--- sticks/sticks.java
+++ sticks/sticks.java
@@ -1,128 +1,162 @@
+// Arup Guha
+// 8/23/07 - Solution to 2007 UCF Local Contest Problem Wall Street
+
+// This problem is basically a different version of the Matrix Chain
+// Multiplication problem with a different evaluation function...
+
 import java.util.*;
+import java.io.*;
 
-public class sticks {
-  static int[][] memo;
-  static int[][] dim;
-  static int n;
+// Stores the dimensions of a lot.
+class Dimensions {
 
-  public static int[][][] subDim;
+	public int length;
+	public int depth;
+	public int cost;
 
-  public static void main(String[] args) {
-    Scanner stdin = new Scanner(System.in);
-    int nCases = stdin.nextInt();
-    // For each case...
-    for (int l = 0; l < nCases; l++) {
-      int length = stdin.nextInt();
-      n = stdin.nextInt();
-      dim = new int[n + 1][2];
+	public Dimensions(int l, int d, int c) {
+		length = l;
+		depth = d;
+		cost = c;
+	}
 
-      // Fugly code to build all the dimensions
-      int[] tmp = new int[n + 2];
-      tmp[0] = 0;
-      int in = 0;
-      int lastIn = 0;
-      for (int i = 1; i <= n ; i++) {
-        lastIn = in;
-        in = stdin.nextInt();
-        tmp[i] = in - lastIn;
-      }
-      tmp[n + 1] = length - in;
-
-      for (int j = 1; j < tmp.length; j++) dim[j - 1][0] = tmp[j];
-      for (int j = 0; j < dim.length; j++) dim[j][1] = 1;
-
-      for (int[] c : dim) {
-        System.out.println(Arrays.toString(c));
-      }
-
-      // Set up memo table.
-			memo = new int[n + 1][n + 1];
-			for (int i=0; i<=n; i++) Arrays.fill(memo[i], -1);
-
-      System.out.println(minMult(0, dim.length - 1));
-    }
+  @Override
+  public String toString() {
+    return "("+length+", "+depth+", "+cost+")";
   }
 
-  // Run the matrix chain multiplication algorithm adapted for this problem.
-  	public int runMCM() {
+}
 
-  		// Will store out intermediate answers to the question:
-  		// What is the minimum cost for fusing some contiguous subsequence of
-  		// lots? It will also store the dimensions of the new lot.
-  		Dimensions[][] minMult = new Dimensions[dim.length][dim.length];
+public class sticks {
+
+	final public static int FUSE_FEE_CONSTANT = 0;
+
+	// A wall street object will store the dimensions of each lot on the street.
+	private Dimensions[] dim;
+
+	// Initialize a wall street object with a Scanner to a file that has
+	// information about each lot on the street.
+	public sticks(Scanner fin) {
+
+		// Get the number of buildings.
+    int lengthOfStick = fin.nextInt();
+		int numbuildings = fin.nextInt();
+		dim = new Dimensions[numbuildings + 1];
+
+    int last = 0;
+		// Read in each building's dimensions.
+		for (int i=0; i< numbuildings; i++) {
+			int len = fin.nextInt();
+			int wid = 1;
+			dim[i] = new Dimensions(len - last, wid, len - last);
+      last = len;
+		}
+    dim[dim.length - 1] = new Dimensions(lengthOfStick - last, 1, lengthOfStick - last);
+
+    System.out.println(Arrays.toString(dim));
+	}
+
+	// Run the matrix chain multiplication algorithm adapted for this problem.
+	public int runMCM() {
+
+		// Will store out intermediate answers to the question:
+		// What is the minimum cost for fusing some contiguous subsequence of
+		// lots? It will also store the dimensions of the new lot.
+		Dimensions[][] minMult = new Dimensions[dim.length][dim.length];
 
 
-  		// Initialize our matrix to show that it takes no cost for the
-  		// individual lots themselves without fusing anything.
-  		for (int i=0; i<minMult.length; i++)
-  			minMult[i][i] = dim[i];
+		// Initialize our matrix to show that it takes no cost for the
+		// individual lots themselves without fusing anything.
+		for (int i=0; i<minMult.length; i++)
+			minMult[i][i] = dim[i];
 
 
-  		// c+1 stands for the number of lots that are put together. In the first
-  		// loop, we are just calculating the cost of putting two lots together.
-  		for (int c = 1; c < minMult.length; c++) {
+		// c+1 stands for the number of lots that are put together. In the first
+		// loop, we are just calculating the cost of putting two lots together.
+		for (int c = 1; c < minMult.length; c++) {
 
-  			for (int o = 0; o < minMult.length-c; o++) {
+			for (int o = 0; o < minMult.length-c; o++) {
 
-  				// This will eventually store the minimum cost for putting
-  				// together lots o through o+c, assuming that we number the lots
-  				// 0 through dim.length-1. The -1 is a flag/sentinel value to
-  				// indicate that we don't have a minimum calculated yet.
-  				Dimensions min = null;
+				// This will eventually store the minimum cost for putting
+				// together lots o through o+c, assuming that we number the lots
+				// 0 through dim.length-1. The -1 is a flag/sentinel value to
+				// indicate that we don't have a minimum calculated yet.
+				Dimensions min = null;
 
-  				// s stands for the boundary at which we are fusing the two
-  				// lots.
-  				for (int s=o; s<o+c; s++) {
+				// s stands for the boundary at which we are fusing the two
+				// lots.
+				for (int s=o; s<o+c; s++) {
 
-  					// Make the calculation for creating the lot by fusing
-  					// at location s. Here are the three components of the cost:
-  					// 1) Cost of first part of the lot.
-  					// 2) Cost of the second part of the lot.
-  					// 3) Cost of fusing those two lots.
+					// Make the calculation for creating the lot by fusing
+					// at location s. Here are the three components of the cost:
+					// 1) Cost of first part of the lot.
+					// 2) Cost of the second part of the lot.
+					// 3) Cost of fusing those two lots.
 
-  					// This is the cost of the two lots being fused.
-  					int value = minMult[o][s].cost + minMult[s+1][o+c].cost;
+					// This is the cost of the two lots being fused.
+					int value = minMult[o][s].cost + minMult[s+1][o+c].cost;
 
-  					// This is the additional cost for the fusing. We are obtaining
-  					// the minimum dimension of each of the two lots.
-  					value += FUSE_FEE_CONSTANT*min(minMult[o][s].length, minMult[o][s].depth)*
-  					                           min(minMult[s+1][o+c].length, minMult[s+1][o+c].depth);
+					// This is the additional cost for the fusing. We are obtaining
+					// the minimum dimension of each of the two lots.
+					value += FUSE_FEE_CONSTANT*min(minMult[o][s].cost, minMult[o][s].depth)*
+					                           min(minMult[s+1][o+c].cost, minMult[s+1][o+c].depth);
 
-  					// These are the length and depth of the fused lot.
-  					int my_length = minMult[o][s].length+minMult[s+1][o+c].length;
-  					int my_depth = max(minMult[o][s].depth, minMult[s+1][o+c].depth);
+					// These are the length and depth of the fused lot.
+					int my_length = minMult[o][s].cost+minMult[s+1][o+c].cost;
+					int my_depth = 1;
 
-  					// No case has been determined, so this is the best seen so far.
-  					if (min == null)
-  						min = new Dimensions(my_length, my_depth, value);
+					// No case has been determined, so this is the best seen so far.
+					if (min == null)
+						min = new Dimensions(my_length, my_depth, my_length);
 
-  					// This case is better than any previous, so update it!
-  					else if (value < min.cost)
-  						min = new Dimensions(my_length, my_depth, value);
-  				}
+					// This case is better than any previous, so update it!
+					else if (my_length < min.cost)
+						min = new Dimensions(my_length, my_depth, my_length);
+				}
 
-  				// Here we want to store the new LOT, with its dimensions and
-  				// minimum cost.
-  				minMult[o][o+c] = min;
+				// Here we want to store the new LOT, with its dimensions and
+				// minimum cost.
+				minMult[o][o+c] = min;
 
-  			}
-  		}
+			}
+		}
 
-  		// This is the answer to our question!
-  		return minMult[0][minMult.length-1].cost;
-  	}
+		// This is the answer to our question!
+		return minMult[0][minMult.length-1].cost;
+	}
 
-  	// Returns the minimum of a and b.
-  	public static int min(int a, int b) {
-  		if (a < b)
-  			return a;
-  		return b;
-  	}
+	// Returns the minimum of a and b.
+	public static int min(int a, int b) {
+		if (a < b)
+			return a;
+		return b;
+	}
 
-  	// Returns the maximum of a and b.
-  	public static int max(int a, int b) {
-  		if (a > b)
-  			return a;
-  		return b;
-  	}
+	// Returns the maximum of a and b.
+	public static int max(int a, int b) {
+		if (a > b)
+			return a;
+		return b;
+	}
+
+
+	public static void main(String[] args) throws Exception {
+
+		// Set up reading from the input file.
+    Scanner stdin = new Scanner(System.in);
+		// Get the number of cases.
+		int numcases = stdin.nextInt();
+
+		// Process each case and print out the corresponding output!
+		for (int my_case=1; my_case<=numcases; my_case++) {
+
+			sticks myBuildings = new sticks(stdin);
+			int my_cost = myBuildings.runMCM();
+			System.out.println(my_cost);
+		}
+
+		// Close the input file.
+		stdin.close();
+	}
+
 }
