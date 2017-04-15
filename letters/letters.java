d3167eeae07fca28514d193cb15e7ac2285d499b
Tim <tim.parkerd@gmail.com>
2017-04-14 22:06:19 -0400

    solution candidate, including debug statements

--- letters/letters.java
+++ letters/letters.java
@@ -8,7 +8,6 @@ public class letters {
   public static void main(String[] args) {
     Scanner stdin = new Scanner(System.in);
     int nCases = Integer.parseInt(stdin.nextLine());
-    if (DEBUG) System.out.println(nCases);
     // For each case...
     for (int c = 0; c < nCases; c++) {
       // Create a new array of all the lists of tiles by type (letter face)
@@ -25,9 +24,9 @@ public class letters {
         Tile newTile = new Tile(tileLine[0].charAt(0), Integer.parseInt(tileLine[1]));
         tiles[tileIndexByCharacter].offer(newTile);
       }
+
       if (DEBUG) System.out.println("--------Tiles--------");
-      for (PriorityQueue l : tiles)
-        if (DEBUG) System.out.println(l);
+      for (PriorityQueue l : tiles) if (DEBUG) System.out.println(l);
 
       // Gather up all the words
       int nWords = Integer.parseInt(stdin.nextLine());
@@ -35,56 +34,57 @@ public class letters {
       for (int w = 0; w < nWords; w++)
         words[w] = stdin.nextLine();
 
-      if (DEBUG) System.out.println(Arrays.toString(words));
-
-      // For each combination of words..
+      // Solve it!
       answer = 0;
       solve(tiles, words, new boolean[words.length], 0);
       System.out.println(answer);
     }
   }
 
+  // Generic function that solves the problem, but it has to loop through
+  // all the possible combinations of the dictionary. It has to try each
+  // combination because if we process first word to last word, there's a
+  // chance that we would never get to the last word, which would be
+  // worth substantially overall.
   @SuppressWarnings("unchecked")
   public static void solve(PriorityQueue[] q, String[] words, boolean[] available, int k) {
     if (k >= available.length) {
-      if (DEBUG) System.out.println("---------Dictionary---------");
-      if (DEBUG) System.out.println(Arrays.toString(available));
-      // System.out.println(process(clone(q), words, available));
+      // DEBUG: Show the current test case with the available dictionary combo
+      if (DEBUG) {
+        if (DEBUG) System.out.println("---------Dictionary---------");
+        for (int b = 0; b < words.length; b++) {
+          if (available[b]) System.out.printf("%s ", words[b]);
+        }
+        System.out.println("\n----------------------------");
+      }
       answer = Math.max(answer, process(clone(q), words, available));
       return;
     }
+    // Flip the word either available or unavailable
     for (int i = 0; i < 2; i++) {
       available[k] = (i % 2 == 0);
       solve(q, words, available, k + 1);
     }
   }
 
+  // The 'meat' of the program that actually decides which tiles to use to
+  // build all the available words in the subset of words in the dictionary
   @SuppressWarnings("unchecked")
-  // Solves the actual board
   public static int process(PriorityQueue[] q, String[] words, boolean[] available) {
     int total = 0;
     // For each known word, try to create it
     for (int w = 0; w < words.length; w++) {
       // Check if the word is available for this combination of the dictionary
       if (!available[w]) continue;
-      if (DEBUG) System.out.println("Case: " + words[w]);
+      if (DEBUG) System.out.println("Build: " + words[w]);
 
       // Look up each letter in the array of PriorityQueues
-      // Make sure to save each one to a temp ArrayList<Tile>
-      // We're use said ArrayList to look back through if the word
-      // cannot be formed. In other words, pop all the letters until
-      // it is either formed or invalid.
-      // If invalid, offer each of the letters back to the array of
-      // PriorityQueues
-      // Otherwise, just move on and add that score to the running total
-      // For each letter in the word...
-
+      // Keep a history of used letters in case of impossible word
       Stack<Tile> processedTiles = new Stack<Tile>();
       int wordTotal = 0;
       for (int c = 0; c < words[w].length(); c++) {
         if (DEBUG) System.out.println("Look up: " + words[w].charAt(c));
-        // Look up letter in the PriorityQueues, if found, add to
-        // the temp ArrayList<Tile>
+
         // Get the first letter
         // Get letter less the 'a' value to get the index
         Tile letter = (Tile)q[words[w].charAt(c) - 'a'].poll();
@@ -93,34 +93,36 @@ public class letters {
         processedTiles.push(letter);
 
         // Case: Word impossible to create
-        // If we cannot get another letter before finishing the word, it is invalid
-        // So, we have to loop back through the ArrayList<Tile> to put them back and
-        // break out of this loop with a total being 0
+        // Can't get all the letters in the word, therefore it's invalid
         if (letter == null) {
           // Remove the null reference at the end b/c it was added to check if null
           processedTiles.pop();
 
-          // Put the wrongly used tiles back into their respective queues/wells
+          // Put the wrongly used tiles back into their respective queues
           // to make them available again
           while (!processedTiles.isEmpty()) {
             Tile tmpTile = processedTiles.pop();
             q[tmpTile.id - 'a'].offer(tmpTile);
           }
-          if (DEBUG) System.out.println(processedTiles);
-          wordTotal = 0;
-          break;
-        }
 
-        // It's safe to process the letter
+          if (DEBUG) System.out.println(processedTiles);
+          // Impossible word is no worthless
+          wordTotal = 0;
+          // Bailout to try next word
+          break;
+        } // End clean up for invalid word
+
         // Increase the value of the word
         wordTotal += letter.value;
+
       } // End letter loop
       if (DEBUG) System.out.println("Word value: " + wordTotal);
+      // Update the running total for this subset of words in the dictionary
       total += wordTotal;
-    } // End words loop
-    if (DEBUG) System.out.println("Words value: " + total);
-    // If we get to this point, we processed the last word
 
+    } // End words loop, so we've processed all words
+
+    if (DEBUG) System.out.println("Words value: " + total);
 
     // Now remove the value of all the remaining letters in the PriorityQueues
     int unusedPoints = 0;
@@ -129,12 +131,17 @@ public class letters {
         unusedPoints += p.poll().value;
       }
     }
-    if (DEBUG) System.out.println("Unused points " + unusedPoints);
 
+    if (DEBUG) System.out.println("Unused points " + unusedPoints);
     if (DEBUG) System.out.println("Solution: " + (total - unusedPoints));
+
+    // Solution is the difference between the sum of used tiles and unused
     return (total - unusedPoints);
   }
 
+  // Because I want to reuse the tiles for a subset of the dictionary,
+  // I need to clone the available tiles, as not to destroy the original
+  // between each case
   @SuppressWarnings("unchecked")
   public static PriorityQueue<Tile>[] clone(PriorityQueue<Tile>[] o) {
     PriorityQueue[] tmp = new PriorityQueue[26];
