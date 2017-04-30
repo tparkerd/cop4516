d1a2e687c70969dbc86bbd36e19c84b3390a6550
Tim <tim.parkerd@gmail.com>
2017-04-30 15:23:10 -0400

    Inventory twist on Knapsack DP

--- hackpack/Knapsack.java
+++ hackpack/Knapsack.java
@@ -1,19 +1,30 @@
 /*
-Example Input
+Example
 
-  long capacity = 20;
-  long[] values = {1, 2, 3, 4, 5};
-  long[] weights = {2, 6, 3, 5, 4};
+- Normal
+  Input
+    long capacity = 20;
+    long[] values = {1, 2, 3, 4, 5};
+    long[] weights = {2, 6, 3, 5, 4};
 
-Example call
+  Call
+    Knapsack sack = new Knapsack(values, weights, capacity);
+    long answer = sack.solve();
 
-Knapsack sack = new Knapsack(values, weights, capacity);
-long answer = sack.solve();
+- Limited Inventory
+  Input
+    long capacity = 20;
+    long[] values = {1, 2, 3, 4, 5};
+    long[] weights = {2, 6, 3, 5, 4};
+    long[] inventory = {1, 1, 1, 2, 5};
 
+  Call
+    Knapsack sack = new Knapsack(values, weights, capacity, inventory);
+    long answer = sack.solve();
 */
 
 public class Knapsack {
-  private long[] values, weights, dp;
+  private long[] values, weights, dp, inventory;
   private long capacity;
 
   public Knapsack(long[] values, long[] weights, long capacity) {
@@ -23,14 +34,33 @@ public class Knapsack {
     this.capacity = capacity;
   }
 
+  // Limited quantity of each
+  public Knapsack(long[] values, long[] weights, long capacity, long[] inventory) {
+    this(values, weights, capacity);
+    this.inventory = inventory.clone(); // Modified, so we have to clone it
+  }
+
   // Get the most you could spend
   // Duplicates allowed
   public long solve() {
     // For each item...
     for (int i = 0; i < values.length; i++) {
-      // for (long j = capacity; j >= weights[i]; j--) { // Backward loop - don't allow duplicates
-      for (long j = weights[i]; j <= capacity; j++) { // Forward loop
+      // for (long j = capacity; j >= weights[i]; j--) { // Don't allow duplicates
+      for (long j = weights[i]; j <= capacity; j++) { // Allow duplicates
         dp[(int)j] = Math.max(dp[(int)j], dp[(int)(j - weights[i])] + values[i]);
+      }
+    }
+    return dp[(int)capacity];
+  }
+
+  // Solve but with a limited quantity of each
+  public long solve() {
+    // For each item...
+    for (int i = 0; i < values.length; i++) {
+      if(inventory[i] <= 0) continue; // skip this item b/c it's exhausted
+      for (long j = weights[i]; j <= capacity; j++) {
+        dp[(int)j] = Math.max(dp[(int)j], dp[(int)(j - weights[i])] + values[i]);
+        inventory[i]--;
       }
     }
     return dp[(int)capacity];
